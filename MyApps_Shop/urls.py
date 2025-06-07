43a0a702c0068781eb5c1a34264e920b71c612bb
narges <narges.shahmoradi.01@gmail.com>
2025-06-07 23:15:46 +0330

    view and url product list and detail

--- MyApps_Shop/urls.py
+++ MyApps_Shop/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shop.urls'), name='shop'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 # Create your views here.
+
+def product_list(request, category_slug = None):
+    category = None
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category)
+    context = {
+        'category': category,
+        'products': products,
+        'categories': categories,
+
+    }
+    return render(request, 'shop/list.html', context)
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug)
+    context = {
+        'product': product,
+    }
+    return render(request, 'shop/detail.html', context)
