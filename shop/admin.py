b4d988808dca07954d665c54e5bb107ab30e3a84
narges <narges.shahmoradi.01@gmail.com>
2025-06-07 23:00:06 +0330

    signals and admin feature

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class ImagInline(admin.TabularInline):
+    model = Imag
+    extra = 0
+
+class FeatureInline(admin.TabularInline):
+    model = ProductFeature
+    extra = 0
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'inventory','new_price', 'created', 'updated']
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ['category', 'created', 'updated']
+    inlines = [ImagInline, FeatureInline]
