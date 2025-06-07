a7f424ed21fbb171a905f83ad6c3f1b396954c62
narges <narges.shahmoradi.01@gmail.com>
2025-06-07 21:49:04 +0330

    add models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name'])]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category' ,args=[self.slug])
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    description = models.TextField(max_length=500)
+    inventory = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
+    weight = models.PositiveIntegerField(default=0)
+    off = models.PositiveIntegerField(default=0)
+    new_price = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created'])
+        ]
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail' ,args=[self.id, self.slug])
+
+
+    def __str__(self):
+        return self.name
+
+
+class ProductFeature(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='features')
+    name = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name + ':' + self.value
+
+class Imag(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    file = models.ImageField(upload_to='post_images/%Y/%m/%d/', blank=True, null=True)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title if self.title else os.path.basename(self.file.url)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+
