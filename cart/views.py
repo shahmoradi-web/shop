36896aa33033d25ea96a9a4f0172279b7709dc72
narges <narges.shahmoradi.01@gmail.com>
2025-06-09 23:35:23 +0330

    add cart with ajax

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
+
+from cart.cart import Cart
+from shop.models import Product
+
 
 # Create your views here.
+
+@require_POST
+def add_to_cart(request, product_id):
+    try:
+        cart = Cart(request)
+        product = get_object_or_404(Product, pk=product_id)
+        cart.add(product)
+        context = {
+            'item_count':len(cart),
+            'total_price':cart.get_total_price()
+        }
+        return JsonResponse(context)
+    except:
+        return JsonResponse({'error':'Something went wrong'})
